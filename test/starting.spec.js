176efcdb88d6c58561c4140c8bfafaa9ed2e229f
Raja <U1192407@ad.us.mrshmc.com>
2019-07-15 11:16:21 +0530

    Should with null values

--- test/starting.spec.js
+++ test/starting.spec.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var should = require('chai').should();
 
 describe('Basic Mocha test', function(){
     it('should deal with objects', function(){
@@ -6,5 +7,10 @@ describe('Basic Mocha test', function(){
         var objB = { name: "Don",  gender: "male"}
         obj.should.have.property('name').equal('Don')
         obj.should.deep.equal(objB)
+    })
+    it('should allow testing nulls',function(){
+        var iAmNull = null;
+        should.not.exist(iAmNull)
+
     })
 })
