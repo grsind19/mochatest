6adbbe537608fa9319239a6657eeca7b7f8cc164
Raja <U1192407@ad.us.mrshmc.com>
2019-07-15 11:06:30 +0530

    Adding should for testing

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert')
 var authcontroller = require('../../controllers/auth.controller')
 var expect = require('chai').expect;
+var should = require('chai').should();
 
 describe('AuthController',()=>{
     beforeEach(function(){
@@ -11,7 +12,13 @@ describe('AuthController',()=>{
         it('should return false if not authorised',()=>{
            var isAuth = authcontroller.isAuthorized('admin')
            expect(isAuth).is.to.be.false
+           should
         })
+        it('should return true if  authorised',()=>{
+            authcontroller.setRoles(['user','admin'])
+            var isAuth = authcontroller.isAuthorized('admin')
+            isAuth.should.be.true
+         })
         it('should not allow get if not authorized')
         it('should allow get if authorized')
     })
